b50c1cb8462aa8bb00065946a65c7dbee400c02e
ruffamaefamat <rmjfamat@gmail.com>
2017-04-21 08:57:01 +0700

    Update GatherInput.java

--- Lab3/GatherInput.java
+++ Lab3/GatherInput.java
@@ -2,45 +2,32 @@ package CMSCLabAns3;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.Console;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class GatherInput {
 	private int size;
 	
 	public void getData(JFrame frame){
-		JLabel label =  new JLabel("Enter Data Size ");
-		JTextField data = new JTextField();
-		JButton cont = new JButton("Continue");
-		data.setColumns(10);
-		
-		Box newBox = Box.createHorizontalBox();
-		newBox.add(label);
-		newBox.add(data);
-		newBox.add(cont);
-		frame.add(newBox);
-		frame.setBounds(300, 400, 200, 100);
-		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
-	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	    frame.pack();
-	    frame.setLocationRelativeTo(null);
-	    frame.setVisible(true);
-	    
-	    cont.addActionListener(new ActionListener()
-	    {
-	      public void actionPerformed(ActionEvent e)
-	      {
+		String input = JOptionPane.showInputDialog("Enter data size:");
+
 	    	try{
-	        size = Integer.parseInt(data.getText());
-	        System.out.println(size);
+	        size = Integer.parseInt(input);
+	        String [] list = new String[size];
+	     
+	        for (int i = 0; i < size; i++){
+	        	String data = JOptionPane.showInputDialog("Enter input " + i);
+	        	list[i] = data;
+	        }
+	        JFrame display = new JFrame();  
+	        JOptionPane.showInputDialog(display, "DATA SET", "Input", JOptionPane.QUESTION_MESSAGE,
+	            null, list, "Titan");
 	    	}
 	    	catch(Exception a){
 	    		JOptionPane.showMessageDialog(null, "Input is invalid");
 	    	}
-	      }
-	    });
+	      
 	}
-
-	
-	
-
 }
